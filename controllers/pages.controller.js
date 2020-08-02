e7c457cf352764eca89d31b61508f1189506db65
kadonomaro <denis.afer@gmail.com>
2020-08-02 14:32:05 +0700

    feat: add navigation partial

--- controllers/pages.controller.js
+++ controllers/pages.controller.js
@@ -2,29 +2,37 @@ const { response } = require("express");
 
 const getIndexPage = async (request, response) => {
 	response.render('index', {
-		title: 'Главная страница',
-		scriptUrl: './assets/js/main.js'
+		title: 'Мыло ручной работы на сайте Handmade soap',
+		description: 'Мыло ручной работы, откройте для себя мир мыловарения. Как сделать мыло своими руками и другие секреты на сайте Handmade soap',
+		scriptUrl: './assets/js/main.js',
+		index: true
 	})
 }
 
 const getBlogPage = async (request, response) => {
 	response.render('blog', {
 		title: 'Творческая мастерская – мыло ручной работы на сайте Handmade soap',
-		scriptUrl: './assets/js/blog.js'
+		description: 'Мыло ручной работы, мыловарение. Как самому можно сделать мыло своими руками, пошаговая инструкция в нашей творческой мастерской на сайте Handmade soap',
+		scriptUrl: './assets/js/blog.js',
+		blog: true
 	})
 }
 
 const getGalleryPage = async (request, response) => {
 	response.render('gallery', {
 		title: 'Галерея работ – мыло ручной работы на сайте Handmade soap',
-		scriptUrl: './assets/js/gallery.js'
+		description: 'Мыло ручной работы, мыловарение. Фотогалерея мыла ручной работы на сайте Handmade soap',
+		scriptUrl: './assets/js/gallery.js',
+		gallery: true
 	})
 }
 
 const getFaqPage = async (request, response) => {
 	response.render('faq', {
 		title: 'Часто задаваемые вопросы – мыло ручной работы на сайте Handmade soap',
-		scriptUrl: './assets/js/faq.js'
+		description: 'Ответы на все вопросы, которые могут возникнуть у начинающего мыловара. Часто задаваемые вопросы на сайте Handmade soap',
+		scriptUrl: './assets/js/faq.js',
+		faq: true
 	})
 }
 
