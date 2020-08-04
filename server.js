b8485c2ff15f0b054e3206206b516a1fd4621db2
kadonomaro <denis.afer@gmail.com>
2020-08-04 19:25:25 +0700

    feat: add mongo connect

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
 const { Router } = require('express')
 const router = Router()
 const handlebars = require('express-handlebars')
 const pagesController = require('./controllers/pages.controller')
 
 const PORT = process.env.PORT || 3000
+const DB_URI = `mongodb+srv://admin:${process.env.DB_PASSWORD}@node.ujzh6.gcp.mongodb.net/handmade_soap`
 
 const app = express()
 const hbs = handlebars.create({
@@ -26,16 +29,26 @@ app.use('/assets', express.static(__dirname + '/assets'))
 app.use(function (req, res) {
 	res.render('404', {
 		title: 'Страница не найдена – Handmade soap'
-	});
-});
-
-
-
-function start() {
-	app.listen(PORT, () => {
-		console.log(`Server running on port: ${PORT}`);
-		console.log(`http://localhost:${PORT}`);
 	})
+})
+
+
+
+async function start() {
+	try {
+		await mongoose.connect(DB_URI, {
+			useNewUrlParser: true,
+			useFindAndModify: true,
+			useUnifiedTopology: true
+		})
+		app.listen(PORT, () => {
+			console.log(`Server running on port: ${PORT}`)
+			console.log(`http://localhost:${PORT}`)
+		})
+	} catch (error) {
+		console.warn(error)
+	}
+
 }
 
 start()
