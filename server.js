b133a5d5dfc445af58efa2736b3b62cb78394e89
kadonomaro <denis.afer@gmail.com>
2020-08-07 20:32:27 +0700

    feat: add typescript

--- server.js
+++ server.js
@@ -22,8 +22,10 @@ router
 	.get('/faq', pagesController.getFaq)
 
 app.engine('hbs', hbs.engine)
+
 app.set('view engine', 'hbs')
 app.set('views', 'pages')
+
 app.use(router)
 app.use('/assets', express.static(__dirname + '/assets'))
 app.use(function (req, res) {
